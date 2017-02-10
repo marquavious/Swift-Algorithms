fe284d0859af57277a31573d6072cfac1681a34a
Marquavious Draggon <marq.draggon@gmail.com>
2017-02-09 20:54:35 -0800

    init

---
+++ AlgorithimsAndWhatnot.playground/Pages/BInarySearch.xcplaygroundpage/Contents.swift
@@ -0,0 +1,71 @@
+//: [Previous](@previous)
+
+import UIKit
+
+var str = "Hello, playground"
+
+// Binary search
+
+var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+func linearSearchForNumbers(searchValue: Int, array: [Int]) -> Bool {
+    var iterations = 0
+    for num in array{
+        iterations += 1
+        if num == searchValue {
+            print("Lame...\(iterations) iterations")
+            return true
+        }
+    }
+    return false
+}
+
+func binarySearchForNumbers(searchValue: Int, array: [Int]) -> Bool {
+    var leftIndex = 0
+    var righIndex = array.count - 1
+    var interatoions = 0
+    while leftIndex <= righIndex {
+        interatoions += 1
+        let middleIndex = (leftIndex + righIndex)/2
+        let middleValue = array[middleIndex]
+        
+        if middleValue == searchValue{
+            print("WOW!! only \(interatoions) iterations!!")
+            return true
+        }
+        
+        if searchValue < middleValue {
+            righIndex = middleIndex - 1
+        }
+        
+        if searchValue > middleIndex{
+            leftIndex = middleIndex+1
+        }
+    }
+    return false
+}
+
+
+var nums = Array(0...10000)
+print("DONE CREATING ARRAY")
+
+
+print("Binary search time: ")
+let methodStart = Date()
+print("Begin", methodStart)
+binarySearchForNumbers(searchValue: 10, array: nums)
+let methodFinish = Date()
+let executionTime = methodFinish.timeIntervalSince(methodStart)
+print("Execution time: \(executionTime)")
+
+
+let methodStart2 = Date()
+print("Lopping through the whole array?")
+print("Begin", methodStart2)
+linearSearchForNumbers(searchValue: 10, array: nums)
+let methodFinish2 = Date()
+let executionTime2 = methodFinish2.timeIntervalSince(methodStart2)
+print("Execution time: \(executionTime2)")
+
+
+//: [Next](@next)
---
+++ Improvemetns.playground/Contents.swift
@@ -0,0 +1,94 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+enum RatingTag {
+    case user
+    case critic
+}
+
+class Movie {
+    var name: String!
+    var tag: RatingTag!
+    var id: Int!
+    var favorited = false
+}
+
+var terribleArray = [Movie]()
+var movieDictionary = [Int:Movie]()
+
+var movieOne = Movie()
+movieOne.id = 1
+movieOne.name = "To Die for"
+movieOne.tag = .user
+movieOne.favorited = false
+movieDictionary[movieOne.id] = movieOne
+terribleArray.append(movieOne)
+
+var movieTwo = Movie()
+movieTwo.id = 1
+movieTwo.name = "To Die for"
+movieTwo.tag = .critic
+movieTwo.favorited = false
+movieDictionary[movieTwo.id] = movieTwo
+terribleArray.append(movieTwo)
+
+var movieThree = Movie()
+movieThree.id = 34
+movieThree.name = "Bout to eat"
+movieThree.tag = .user
+movieTwo.favorited = false
+movieDictionary[movieThree.id] = movieThree
+terribleArray.append(movieThree)
+
+var movieFour = Movie()
+movieFour.id = 59
+movieFour.name = "Surfin fam"
+movieFour.tag = .user
+movieTwo.favorited = false
+movieDictionary[movieFour.id] = movieFour
+terribleArray.append(movieFour)
+
+var movieFive = Movie()
+movieFive.id = 398
+movieFive.name = "EATING PIE"
+movieFive.tag = .critic
+movieTwo.favorited = false
+movieDictionary[movieFive.id] = movieFive
+terribleArray.append(movieFive)
+
+
+func addToFavorites(movie:Movie){
+    if movie.favorited == true {
+        movie.favorited = false
+    }else{
+        movie.favorited = true
+    }
+}
+
+print(movieOne.favorited)
+addToFavorites(movie: movieDictionary[movieOne.id]!)
+print(movieTwo.favorited)
+
+
+// Not using Dictionaries or whatever
+func badAddToFavorites(movie:Movie){
+    for movieToFind in terribleArray{
+        if movieToFind.id == movie.id {
+            movie.favorited = true
+        }
+    }
+}
+
+//
+//for (movie,name) in movieDictionary {
+//    print(movie,name.name)
+//}
+
+print(movieDictionary[movieFour.id]!.name)
+
+
+
+
